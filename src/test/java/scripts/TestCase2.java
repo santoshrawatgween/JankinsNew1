05a6b8a775803304fc239ac3a8ea143e1b9cd94a
santo <santo@192.168.1.38>
2025-06-02 19:56:30 +0530

    first

---
+++ src/test/java/scripts/TestCase2.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class TestCase2 
+{
+	@Test
+	public void tc2()
+	{
+		Reporter.log("Hello Selenium", true);
+	}
+
+}
---
+++ src/test/java/scripts/TestCase3.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase3 
+{
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Hello Api", true);
+	}
+
+}
